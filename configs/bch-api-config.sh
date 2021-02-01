8f8b0a04b352243c230afe6bf88a8e8da9202926
nach <napat_pat3@hotmail.com>
2021-02-01 21:36:25 +0700

    feat: multi op node

--- configs/bch-api-config.sh
+++ configs/bch-api-config.sh
@@ -15,8 +15,8 @@ export BLOCKBOOK_URL=https://<Blockbook IP>:9131/
 export NODE_TLS_REJECT_UNAUTHORIZED=0
 
 # Mainnet Fulcrum / ElectrumX
-export FULCRUM_URL=localhost
-export FULCRUM_PORT=50002
+export FULCRUM_URL=bitcoind-regtest
+export FULCRUM_PORT=50001
 
 export SECURITY=false
 ### require if SECURITY=true ###
