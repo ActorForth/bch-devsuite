b37c99793270d9ab3d663bf9e6a9a27dd4b391c3
Amp <amp@biggestfan.net>
2021-12-09 16:59:04 +0700

    Ensure output before write

--- docs/index.js
+++ docs/index.js
@@ -8,6 +8,8 @@ const docs_compiled_template = hb.compile(docs_template.toString());
 const readme = fs.readFileSync("../README.md");
 const readme_html = md.render(readme.toString());
 
+fs.ensureFileSync("_site/README/index.html");
+
 fs.writeFileSync(
   "_site/README/index.html",
   docs_compiled_template({ content: new hb.SafeString(readme_html) })
---
+++ docs/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ["./templates/**/*.html", "./_site/**/*.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
