684844ac863a8ef88a3cc7ee835d0619cd75f849
Amp <amp@biggestfan.net>
2021-12-09 18:37:31 +0700

    Add tailwind css

--- docs/index.js
+++ docs/index.js
@@ -1,16 +1,21 @@
 const fs = require("fs-extra");
 const hb = require("handlebars");
 const md = require("markdown-it")();
+const exec = require("exec-sh").promise;
 
-const docs_template = fs.readFileSync("templates/docs.html");
-const docs_compiled_template = hb.compile(docs_template.toString());
+(async () => {
+  const docs_template = fs.readFileSync("templates/docs.html");
+  const docs_compiled_template = hb.compile(docs_template.toString());
 
-const readme = fs.readFileSync("../README.md");
-const readme_html = md.render(readme.toString());
+  const readme = fs.readFileSync("../README.md");
+  const readme_html = md.render(readme.toString());
 
-fs.ensureFileSync("_site/README/index.html");
+  fs.ensureFileSync("_site/README/index.html");
 
-fs.writeFileSync(
-  "_site/README/index.html",
-  docs_compiled_template({ content: new hb.SafeString(readme_html) })
-);
+  fs.writeFileSync(
+    "_site/README/index.html",
+    docs_compiled_template({ content: new hb.SafeString(readme_html) })
+  );
+
+  await exec("npm run build:css");
+})();
