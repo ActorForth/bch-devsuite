9e7a539b11c89e4f6ea2c406eb27d789df9965e5
Amp <amp@biggestfan.net>
2021-12-14 21:04:47 +0700

    Attach id to h2

--- docs/index.js
+++ docs/index.js
@@ -2,10 +2,8 @@ const fs = require("fs-extra");
 const hb = require("handlebars");
 const md = require("markdown-it")();
 const exec = require("exec-sh").promise;
-const DomParser = require("dom-parser");
+const { JSDOM } = require("jsdom");
 const slugify = require("slugify");
-
-const domParser = new DomParser();
 
 (async () => {
   const docs_template = fs.readFileSync("templates/docs.html");
@@ -14,12 +12,12 @@ const domParser = new DomParser();
   const readme = fs.readFileSync("../README.md");
   const readme_html = md.render(readme.toString());
 
-  fs.ensureFileSync("_site/index.html");
+  // fs.ensureFileSync("_site/index.html");
 
-  fs.writeFileSync(
-    "_site/index.html",
-    docs_compiled_template({ content: new hb.SafeString(readme_html) })
-  );
+  // fs.writeFileSync(
+  //   "_site/index.html",
+  //   docs_compiled_template({ content: new hb.SafeString(readme_html) })
+  // );
 
   writePages(docs_compiled_template, readme_html);
 
@@ -31,12 +29,16 @@ function writePages(template, html) {
     .split("<h1>")
     .filter((x) => !!x)
     .map((x) => {
-      const content = `<h1>${x}`;
-      const dom = domParser.parseFromString(content);
-      const page_title = dom.getElementsByTagName("h1")[0].textContent;
+      const content = `<root><h1>${x}</root>`;
+      const dom = JSDOM.fragment(content);
+      const page_title = dom.querySelector("h1").textContent;
       const page_slug = slugify(page_title, { lower: true });
       const file_name = `_site/${page_slug}.html`;
-      const submenus = dom.getElementsByTagName("h2").map((h2) => {
+      const h2_nodes = Array.from(dom.querySelectorAll("h2"));
+      h2_nodes.forEach((node) => {
+        node.setAttribute("id", slugify(node.textContent, { lower: true }));
+      });
+      const submenus = h2_nodes.map((h2) => {
         return {
           title: h2.textContent,
         };
@@ -45,7 +47,7 @@ function writePages(template, html) {
         name: page_title,
         slug: page_slug,
         file_name: file_name,
-        content,
+        content: dom.childNodes[0].innerHTML,
         submenus,
       };
     });
